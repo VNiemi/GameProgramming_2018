113768daa94e60a3c8e2c21f99253adc0e8f4602
VNiemi <ville.niemi@cs.tamk.fi>
2018-02-05 12:42:55 +0200

    no message

--- TankGame/Assets/Code/Tests/GetComponentInHierarchyTester.cs
+++ TankGame/Assets/Code/Tests/GetComponentInHierarchyTester.cs
@@ -1,42 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-namespace TankGame.Testing {
-public class GetComponentInHierarchyTester : MonoBehaviour {
-
-    private bool _includeInactive;
-
-	public void Setup(bool includeInactive, bool componentInParent, bool setActive)
-    {
-        _includeInactive = includeInactive;
-
-        GameObject go;
-
-        if (componentInParent)
-        {
-            go = transform.parent.gameObject;
- 
-        }
-        else
-        {
-            go = transform.GetChild(0).gameObject;
-
-        }
-
-        go.AddComponent<TestComponent>();
-        go.SetActive(setActive);
-    }
-
-        public TestComponent Run()
-        {
-            return gameObject.GetComponentInHierarchy<TestComponent>(_includeInactive);
-        }
-}
-
-
-=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -69,5 +31,4 @@ namespace TankGame.Testing
 			return gameObject.GetComponentInHierarchy< TestComponent >( _includeInactive );
 		}
 	}
->>>>>>> sami_branch/master
 }

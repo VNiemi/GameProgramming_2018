4fa25d65b21b33c64b2ca709a98bb66839afe954
VNiemi <ville.niemi@cs.tamk.fi>
2018-02-05 12:45:06 +0200

    Trying to fix the project.

--- TankGame/Assets/Code/Editor/GetOrAddComponentTest.cs
+++ TankGame/Assets/Code/Editor/GetOrAddComponentTest.cs
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-﻿using UnityEngine;
-using UnityEditor;
-using UnityEngine.TestTools;
-using NUnit.Framework;
-using System.Collections;
-
-namespace TankGame.Testing
-{
-
-    public class GetOrAddComponentTest
-    {
-
-        [Test]
-        public void GetOrAddComponentTestSimple()
-        {
-            GameObject go = new GameObject();
-            TestComponent component = go.GetOrAddComponent<TestComponent>();
-            Assert.NotNull(component);
-        }
-
-        [Test]
-        public void GetOrAddComponentTestSingleGO()
-        {
-            GameObject go = new GameObject();
-            go.GetOrAddComponent<TestComponent>();
-            go.GetOrAddComponent<TestComponent>();
-            TestComponent[] components = go.GetComponents<TestComponent>();
-            Assert.IsTrue(components.Length == 1);
-        }
-
-    }
-=======
 using NUnit.Framework;
 using UnityEngine;
 
@@ -58,5 +25,4 @@ namespace TankGame.Testing
 			Assert.IsTrue( components.Length == 1 );
 		}
 	}
->>>>>>> sami_branch/master
 }
